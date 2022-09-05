57dfeebaff6e7d9bede1bbb572cfd41a3949e069
unknown <wallaceomatthew@gmail.com>
2022-09-05 14:38:02 -0700

    features update

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './whatgpt3.css';
+import Feature from './../../components/feature/Feature';
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className='gpt3__whatgpt3 section__margin' id='whpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature title={"Whats is GPT3"}  text={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}/>
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient__text'> Possibility beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+          <Feature  title={"Chatbots"} text={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}/>
+          <Feature  title={"Knowledgebase"} text={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}/>
+          <Feature title={"Education"} text={"Lorem Ipsum is simply dummy text of the printing and typesetting industry."}/>
+      </div>
+    </div>
   )
 }
 
